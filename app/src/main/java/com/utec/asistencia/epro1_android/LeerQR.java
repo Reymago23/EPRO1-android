912fefe998fa3249cd9612c78eab8826514108f7
Miguel Henriquez <elderriqui@gmail.com>
2018-09-23 08:39:21 -0600

    initial commit with log in design and all activities

---
+++ app/src/main/java/com/utec/asistencia/epro1_android/LeerQR.java
@@ -0,0 +1,13 @@
+package com.utec.asistencia.epro1_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LeerQR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leer_qr);
+    }
+}
